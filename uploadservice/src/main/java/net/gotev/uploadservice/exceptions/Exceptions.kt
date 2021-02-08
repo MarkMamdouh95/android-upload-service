a59f55d9ee6f28b9298ad114d5afea649134b6c3
KHALED-PC\ADMIN <khaled.aboshama@pharos-solutions.de>
2021-02-08 15:20:38 +0200

    Add parameter of exception

--- uploadservice/src/main/java/net/gotev/uploadservice/exceptions/Exceptions.kt
+++ uploadservice/src/main/java/net/gotev/uploadservice/exceptions/Exceptions.kt
@@ -1,6 +1,7 @@
 package net.gotev.uploadservice.exceptions
 
 import net.gotev.uploadservice.UploadThrowable
+import java.lang.Exception
 
-class UserCancelledUploadException(httpMethod: String) : UploadThrowable(message = "User cancelled upload", httpMethod = httpMethod)
-class UploadError(httpMethod: String) : UploadThrowable(message = "Upload error", httpMethod = httpMethod)
+class UserCancelledUploadException(httpMethod: String, exception: Exception? = null) : UploadThrowable(message = "User cancelled upload", httpMethod = httpMethod, exception)
+class UploadError(httpMethod: String, exception: Exception? = null) : UploadThrowable(message = "Upload error", httpMethod = httpMethod, exception = exception)
